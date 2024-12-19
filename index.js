5ffebd77d47e48e9ae42435cd866230c4e3b96b8
Byambasuren11 <byumbasurenbold@gmail.com>
2024-12-19 18:10:23 +0800

    mm

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 let contains = document.getElementById("maincontain");
 let baruundeed = document.createElement("button");
 baruundeed.id = "right-top";
+baruundeed.key = "4";
 let zuundeed = document.createElement("button");
 zuundeed.id = "left-top";
+zuundeed.key = "3";
 let baruundood = document.createElement("button");
 baruundood.id = "right-bottom";
+baruundood.key = "2";
 let zuundood = document.createElement("button");
 zuundood.id = "left-bottom";
-document.getElementById("maincontain").appendChild(baruundeed);
+zuundood.key = "1";
+const buttonArray = [baruundeed, zuundeed, zuundood, baruundood];
+const baruundeed1 = document
+  .getElementById("maincontain")
+  .appendChild(baruundeed);
 document.getElementById("maincontain").appendChild(zuundeed);
 document.getElementById("maincontain").appendChild(baruundood);
 document.getElementById("maincontain").appendChild(zuundood);
@@ -27,73 +34,84 @@ midCircle_dund.id = "midCircle_dund";
 midCircle_dund.innerHTML = "start";
 let midCircle_dood = document.createElement("div");
 midCircle_dood.id = "midCircle_dood";
-// midCircle_dood.type="number";
-// midCircle_dood.min="1";
-// midCircle_dood.max="5"
-baruundeed = 1;
-baruundood = 2;
-zuundeed = 3;
-zuundood = 4;
+
 document.getElementById("midCircle").appendChild(midCircle_garchig);
 document.getElementById("midCircle").appendChild(midCircle_dund);
 document.getElementById("midCircle").appendChild(midCircle_dood);
 
 midCircle_dund.addEventListener("click", startFunction);
+let random = [];
+let current = 0;
+let previous=[];
+let start=false;
+let i=1;
 function startFunction() {
-  midCircle_dood.innerHTML = "1";
-  let random = [];
+  midCircle_dood.innerHTML = i++;
   let randomToo = Math.floor(Math.random() * 4) + 1;
-  clickButton();
-  console.log(randomToo)
-  // do {
-  //   let randomToo = Math.floor(Math.random() * 4) + 1;
-  //   if (randomToo === 4) {
-  //     document.getElementById("right-top").style.animation =
-  //       "ajillah4 1s linear";
-  //   } else if (randomToo === 3) {
-  //     document.getElementById("left-top").style.animation =
-  //       "ajillah3 1s linear";
-  //   } else if (randomToo === 2) {
-  //     document.getElementById("right-bottom").style.animation =
-  //       "ajillah2 1s linear";
-  //   } else if (randomToo === 1) {
-  //     document.getElementById("left-bottom").style.animation =
-  //       "ajillah1 1s linear";
-  //   }
-  //   random.push(randomToo);
-  //   clickButton();
-  //   console.log(random);
-  // } while (random.length < 5);
-  // {
-  //   console.log("hello world");
-  // }
+  current = randomToo;
+  turnOnColor(current);
+  random.push(randomToo);
+//setTimeout();
+  // console.log(previous);
 }
-//console.log(baruundeed)
-function clickButton(event) {
-  document.getElementById("right-top").addEventListener("click", had);
-  document.getElementById("left-top").addEventListener("click", had);
-  document.getElementById("right-bottom").addEventListener("click", had);
-  document.getElementById("left-bottom").addEventListener("click", had);
-  //let b=event.target;
+function turnOnColor(randomToo) {
+  if (randomToo === 4) {
+    document.getElementById("right-top").style.animation = "ajillah4 1s linear";
+    setTimeout(() => {
+      document.getElementById("right-top").style.removeProperty("animation");
+    }, 1000);
+  } else if (randomToo === 3) {
+    document.getElementById("left-top").style.animation = "ajillah3 1s linear";
+    setTimeout(() => {
+      document.getElementById("left-top").style.removeProperty("animation");
+    }, 1000);
+  } else if (randomToo === 2) {
+    document.getElementById("right-bottom").style.animation =
+      "ajillah2 1s linear";
+    setTimeout(() => {
+      document.getElementById("right-bottom").style.removeProperty("animation");
+    }, 1000);
+  } else if (randomToo === 1) {
+    document.getElementById("left-bottom").style.animation =
+      "ajillah1 1s linear";
+    setTimeout(() => {
+      document.getElementById("left-bottom").style.removeProperty("animation");
+    }, 1000);
+  }
 }
-function had() {
-  if (zuundood === durem) {
-    console.log(true);
+//setTimeout(startFunction,500);
+buttonArray.forEach((value, index) => {
+  function buttonClick() {
     
-    buttonArray = [];
-    buttonArray.push();
-  } 
-  else {
-    console.log("buruu bna");
-    
+    if (Number(value.key) === random[random.length - 1]) {
+      previous.push(value.key);
+      if(random.length === previous.length){ 
+        if(random[index]==previous[index]){
+
+        }
+
+      }
+
+      console.log(random);
+      console.log(previous);
+      
+      
+      startFunction()
+      // if(previous==random[j]){
+      //         startFunction();
+      // }
+      // else {
+      //   console.log("buruu bllaa");
+        
+      // }
+
+
+    }
+    else {
+      alert("buruu towch darlaa")
+    }
   }
 
+  value.addEventListener("click", buttonClick);
+});
 
-}
-
-// document.getElementById("right-top").style.animation="ajillah 1s linear";
-
-// zuundood=4
-// if(midCircle_dund===4) {
-//     zuundood.style.backgroundColor="white";
-// }
